8655fe57545d78032acfd2266fbb8a76d5e3026d
Saloncey <jakukaj@gmail.com>
2023-05-31 20:41:00 +0200

    start

---
+++ src/main/java/saloncey/d4jmmorpg/D4jmmorpgApplication.java
@@ -0,0 +1,13 @@
+package saloncey.d4jmmorpg;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class D4jmmorpgApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(D4jmmorpgApplication.class, args);
+	}
+
+}
---
+++ src/test/java/saloncey/d4jmmorpg/D4jmmorpgApplicationTests.java
@@ -0,0 +1,13 @@
+package saloncey.d4jmmorpg;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class D4jmmorpgApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
